40bf226e4617f3dfbfbec9e355cdb5675cd1133e
Jeff <93086727+Jeffersfaria@users.noreply.github.com>
2023-06-07 20:45:59 -0400

    Update try e catch em C#.cs

--- try e catch em C#.cs
+++ try e catch em C#.cs
@@ -36,3 +36,73 @@ finally
 
 //Em resumo, o "try" e "catch" em C# são usados para capturar e tratar exceções, permitindo que o código lide com erros de forma controlada e execute ações apropriadas em caso de falhas.
 //Isso ajuda a melhorar a robustez e a confiabilidade do programa.
+
+***********************************************************************************************************************************************************************************************
+
+
+//Exemplos de como utilizar as propriedades "Message", "StackTrace" e "InnerException" em C#:
+
+//Exemplo utilizando a propriedade "Message":
+
+
+try
+{
+    int[] numbers = new int[3];
+    Console.WriteLine(numbers[4]);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ocorreu um erro: " + ex.Message);
+}
+
+
+//Nesse exemplo, um erro de índice é gerado ao tentar acessar o quarto elemento de um array de tamanho 3. A propriedade "Message" é utilizada para exibir uma mensagem de erro que descreve o problema.
+
+//Exemplo utilizando a propriedade "StackTrace":
+
+
+try
+{
+    throw new Exception("Ocorreu um erro.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ocorreu uma exceção: " + ex.StackTrace);
+}
+
+
+//Nesse exemplo, uma exceção é lançada manualmente com uma mensagem de erro. A propriedade "StackTrace" é utilizada para obter uma representação textual da pilha de chamadas de métodos que levou à ocorrência
+//da exceção.
+
+//Exemplo utilizando a propriedade "InnerException":
+
+
+try
+{
+    try
+    {
+        throw new ArgumentException("Argumento inválido.");
+    }
+    catch (Exception innerEx)
+    {
+        throw new InvalidOperationException("Operação inválida.", innerEx);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ocorreu uma exceção: " + ex.Message);
+    if (ex.InnerException != null)
+    {
+        Console.WriteLine("Exceção interna: " + ex.InnerException.Message);
+    }
+}
+
+
+//Nesse exemplo, um bloco "try" aninhado é utilizado para capturar uma exceção inicial ("ArgumentException") e lançar uma nova exceção ("InvalidOperationException") com a exceção original como a exceção 
+//interna. A propriedade "InnerException" é utilizada para acessar a exceção interna e exibir sua mensagem de erro.
+
+//Esses exemplos demonstram como utilizar as propriedades "Message", "StackTrace" e "InnerException" para obter informações úteis sobre as exceções ocorridas durante a execução do código.
+
+
+
+
